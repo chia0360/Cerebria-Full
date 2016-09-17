bd3258271809faa8798354d872475f30186cd8e9
chia0360 <chia0360@e.ntu.edu.sg>
2016-09-17 21:14:33 +0800

    added partial files

---
+++ android/src/com/oracledragon/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.oracledragon.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.oracledragon.game.Cerebria;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Cerebria(), config);
+	}
+}
---
+++ desktop/src/com/oracledragon/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.oracledragon.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.oracledragon.game.Cerebria;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Cerebria(), config);
+	}
+}
